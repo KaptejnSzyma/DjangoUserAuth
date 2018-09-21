726e88fe314c689133297ffd266090a2eed149ab
Szymek <szymon.rzadca@gmail.com>
2018-09-21 17:45:34 +0200

    urls setup

--- BasicApp/urls.py
+++ BasicApp/urls.py
@@ -4,5 +4,6 @@ from BasicApp import views
 app_name = 'BasicApp'
 
 urlpatterns = [
-    url(r'^register/$', views.register, name='register')
+    url(r'^register/$', views.register, name='register'),
+    url(r'^user_login/', views.user_login, name='user_login')
 ]
--- DjangoUserAuth/urls.py
+++ DjangoUserAuth/urls.py
@@ -20,5 +20,7 @@ from BasicApp import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
-    url(r'^basicapp/', include('BasicApp.urls'))
+    url(r'^basicapp/', include('BasicApp.urls')),
+    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^special/', views.special, name='special')
 ]
